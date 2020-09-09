f417de034fc30321cb2b5509b5c39fd665d705ba
Sword-of-GraySkull <70310742+Sword-of-GraySkull@users.noreply.github.com>
2020-09-09 13:23:13 +0530

    Add files via upload

---
+++ build/precache-manifest.36572eb72b82047d5b668fd764e6057c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83fcf0f805cb1cd5b88ca3d0090fb268",
+    "url": "/swag-shop-web/index.html"
+  },
+  {
+    "revision": "79733877a17232530981",
+    "url": "/swag-shop-web/static/css/main.dea77f71.chunk.css"
+  },
+  {
+    "revision": "3b7f4d695f122513a60a",
+    "url": "/swag-shop-web/static/js/2.d78ded81.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/swag-shop-web/static/js/2.d78ded81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79733877a17232530981",
+    "url": "/swag-shop-web/static/js/main.d147dd11.chunk.js"
+  },
+  {
+    "revision": "d6b29239d67b90d65104",
+    "url": "/swag-shop-web/static/js/runtime-main.ac22ad79.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/swag-shop-web/static/media/logo.ee7cd8ed.svg"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,88 @@
+var express = require('express');
+var app = express();
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+useNewUrlParser = true;
+var db = mongoose.connect('mongodb://localhost/swag-shop', { useNewUrlParser: true, useUnifiedTopology: true });
+
+var Product = require('./model/product');
+var WishList = require('./model/wishlist');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(cors());
+
+app.post("/product", function( request, response) {
+    var product = new Product();
+    product.title = request.body.title;
+    product.price = request.body.price;
+    product.save(function(err, savedProduct) {
+       if( err) {
+           response.status(500).send({error: "Could not save product"});
+       } 
+       else {
+           response.status(200).send(savedProduct);
+       }
+    });
+});
+
+app.post("/wishlist", function( request, response) {
+    var wishList = new WishList();
+    wishList.title = request.body.title;
+    
+    wishList.save(function(err, newWishList) {
+        if(err) {
+            response.status(500).send({error: "Could not save Wishlist"});
+        }
+        else {
+            response.status(200).send( newWishList);
+        }
+    });
+});
+
+app.put('/wishlist/product/add', function(request, response) {
+    Product.findOne({_id: request.body.productId}, function(err, product) {
+        if(err) {
+            response.status(500).send({error:"Could not add item to wishlist"});
+        }
+        else {
+            WishList.update({_id: request.body.wishListId}, {$addToSet: {products: product._id}}, function (err, wishList) {
+                if(err) {
+                    response.status(500).send({error:"Could not add item to wishlist"});
+                }
+                else {
+                response.send("Successfully added to the Wishlist");
+                }   
+            })
+        }
+    })
+})
+
+app.get("/wishlist", function( request, response) {
+    WishList.find({}).populate({path:'products', model: 'Product'}).exec(function(err, wishlist) {
+        if(err) {
+            response.status(500).send({error:"Could not get the Wishlist"});
+        }
+        else {
+            response.status(200).send(wishlist);
+        }
+    })
+})
+
+app.get("/product", function( request, response) {
+    Product.find({}, function(err, products) {
+        if( err) {
+            response.status(500).send({error: "Could not fetch products"});
+        }
+        else {
+            response.send(products);
+        }
+    })
+});
+
+app.listen(3001, function() {
+    console.log("Swag Shop API running on port 3001!!")
+});
+
